55a6e34bfdd12fe8ba57dfc17732d142027d3c25
Sebastien Aperghis-Tramoni <sebastien@aperghis.net>
2008-11-10 16:30:17 +0100

    Module used by the tests.

---
+++ t/Utils.pm
@@ -0,0 +1,31 @@
+use strict;
+use Carp;
+
+sub new_string_fh {
+    my @handles = ();
+    my $i = 1;
+
+    for my $stringref (@_) {
+        croak "error: argument $i is not a scalarref"
+            unless ref $stringref eq "SCALAR";
+
+        my $fh = undef;
+
+        if ($] < 5.008) {
+            require IO::String;
+            $fh = IO::String->new($stringref);
+        }
+        else {
+            open($fh, "<", $stringref)
+                or croak "fatal: Can't read in-memory buffer: $!";
+        }
+
+        $i++;
+        push @handles, $fh;
+    }
+
+    return @handles
+}
+
+
+1
