919f5581962e34e54fcde94c4e4cd7513bbe9ce1
Sebastien Aperghis-Tramoni <sebastien@aperghis.net>
2011-08-12 14:26:20 +0200

    set release date and version.

--- lib/SNMP/Extension/PassPersist.pm
+++ lib/SNMP/Extension/PassPersist.pm
@@ -17,7 +17,7 @@ use Sys::Syslog;
 
 {
     no strict "vars";
-    $VERSION = '0.05';
+    $VERSION = '0.06';
 }
 
 use constant HAVE_SORT_KEY_OID
@@ -544,7 +544,7 @@ for Net-SNMP
 
 =head1 VERSION
 
-This is the documentation of C<SNMP::Extension::PassPersist> version 0.05
+This is the documentation of C<SNMP::Extension::PassPersist> version 0.06
 
 
 =head1 SYNOPSIS
